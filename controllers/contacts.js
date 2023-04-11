8be0508e08076dfcbace43b54490355237af9401
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-04-11 17:41:13 +0300

    add favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,11 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite = true} = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
+    favorite,
   }).populate("owner", "email");
   res.json(result);
 };
