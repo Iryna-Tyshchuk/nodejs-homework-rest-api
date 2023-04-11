eb2501f6a5caf8e02b3b6a0a3ca3f9c1da3bf7a3
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-04-11 18:05:17 +0300

    fix favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,13 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20, favorite = true} = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const result = await Contact.find(favorite ? { owner, favorite } : { owner }, "-createdAt -updatedAt", {
     skip,
     limit,
-    favorite,
-  }).populate("owner", "email");
+  }).populate("owner", "email subscription");
   res.json(result);
 };
 
